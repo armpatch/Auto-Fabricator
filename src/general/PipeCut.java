845a7eebcf57b303d90b9660518b2df44997be31
Aaron <ampatch@gmail.com>
2019-08-27 20:51:14 -0400

    PipeCuts: removed unnecessary fields, added a rowData String[] field

--- src/general/PipeCut.java
+++ src/general/PipeCut.java
@@ -3,52 +3,27 @@ package general;
 import java.util.UUID;
 
 public class PipeCut {
+
     private UUID uuid;
 
-    private int jobNumber;
-    private String spool;
-    private String itemNumber;
-    private String area;
+    private String[] rowData;
     private String service;
-    private String material;
-    private String end1;
-    private String lengthFractional;
-    private String connector1;
-    private String connector2;
     private int rawLength;
-    private String printer;
-    private String bin;
 
-    public int getJobNumber() {
-        return jobNumber;
+    public PipeCut() {
+        uuid = UUID.randomUUID();
     }
 
-    public void setJobNumber(int jobNumber) {
-        this.jobNumber = jobNumber;
+    public UUID getUuid() {
+        return uuid;
     }
 
-    public String getSpool() {
-        return spool;
+    public String[] getRowData() {
+        return rowData;
     }
 
-    public void setSpool(String spool) {
-        this.spool = spool;
-    }
-
-    public String getItemNumber() {
-        return itemNumber;
-    }
-
-    public void setItemNumber(String itemNumber) {
-        this.itemNumber = itemNumber;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
+    public void setRowData(String[] rowData) {
+        this.rowData = rowData;
     }
 
     public String getService() {
@@ -59,46 +34,6 @@ public class PipeCut {
         this.service = service;
     }
 
-    public String getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
-    }
-
-    public String getEnd1() {
-        return end1;
-    }
-
-    public void setEnd1(String end1) {
-        this.end1 = end1;
-    }
-
-    public String getLengthFractional() {
-        return lengthFractional;
-    }
-
-    public void setLengthFractional(String lengthFractional) {
-        this.lengthFractional = lengthFractional;
-    }
-
-    public String getConnector1() {
-        return connector1;
-    }
-
-    public void setConnector1(String connector1) {
-        this.connector1 = connector1;
-    }
-
-    public String getConnector2() {
-        return connector2;
-    }
-
-    public void setConnector2(String connector2) {
-        this.connector2 = connector2;
-    }
-
     public int getRawLength() {
         return rawLength;
     }
@@ -107,33 +42,7 @@ public class PipeCut {
         this.rawLength = rawLength;
     }
 
-    public String getPrinter() {
-        return printer;
-    }
 
-    public void setPrinter(String printer) {
-        this.printer = printer;
-    }
 
-    public String getBin() {
-        return bin;
-    }
 
-    public void setBin(String bin) {
-        this.bin = bin;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    private String label;
-
-    public PipeCut() {
-        uuid = UUID.randomUUID();
-    }
 }
