2e68c441cc7f7a164e4fb816775fc40f2e5c5207
Aaron <ampatch@gmail.com>
2019-08-27 20:51:52 -0400

    PipeCutFactory: removed unused setters for PipeCutMaker

--- src/general/pipeCutFactory.java
+++ src/general/pipeCutFactory.java
@@ -10,31 +10,21 @@ public class pipeCutFactory {
     private static final String pathToCsv = "C:\\Users\\Aaron\\IdeaProjects\\Pipe Cutter Algo\\" +
                     "research\\pipe csvs\\Original\\pipes.csv";
 
-    public static ArrayList<PipeCut> getPipeCuts(ArrayList<String> rows) {
+    public static ArrayList<PipeCut> getPipeCutsFrom(ArrayList<String> rawCSVRows) {
         final int FIRST_ROW = 1;
 
-        ArrayList<PipeCut> pipeCuts = new ArrayList<>(rows.size());
+        ArrayList<PipeCut> pipeCuts = new ArrayList<>(rawCSVRows.size());
 
-        for (int arrayIndex = FIRST_ROW; arrayIndex < rows.size(); arrayIndex++) {
-            String rowString = rows.get(arrayIndex);
+        for (int arrayIndex = FIRST_ROW; arrayIndex < rawCSVRows.size(); arrayIndex++) {
+            String rowString = rawCSVRows.get(arrayIndex);
 
             String[] pipeCutData = rowString.split(",");
 
             PipeCut pipeCut = new PipeCut();
 
-            pipeCut.setJobNumber(Integer.valueOf(pipeCutData[0]));
-            pipeCut.setSpool(pipeCutData[1]);
-            pipeCut.setItemNumber(pipeCutData[2]);
-            pipeCut.setArea(pipeCutData[3]);
+            pipeCut.setRowData(rowString.split(","));
             pipeCut.setService(pipeCutData[4]);
-            pipeCut.setMaterial(pipeCutData[5]);
-            pipeCut.setEnd1(pipeCutData[6]);
-            pipeCut.setLengthFractional(pipeCutData[7]);
-            pipeCut.setConnector1(pipeCutData[8]);
-            pipeCut.setConnector2(pipeCutData[9]);
             pipeCut.setRawLength(Integer.valueOf(pipeCutData[10]));
-            pipeCut.setPrinter(pipeCutData[11]);
-            pipeCut.setBin(pipeCutData[12]);
 
             pipeCuts.add(pipeCut);
         }
@@ -42,7 +32,7 @@ public class pipeCutFactory {
         return pipeCuts;
     }
 
-    static ArrayList<String> getRowsFromCSV() throws IOException {
+    static ArrayList<String> getRowsFromCSV(String path) throws IOException {
         BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
 
         String row;
