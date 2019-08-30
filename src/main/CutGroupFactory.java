6435dcef172e44019adf7deaff0535b24eeb88f8
Aaron <ampatch@gmail.com>
2019-08-30 09:17:30 -0400

    CutGroupFactory clean up

--- src/main/CutGroupFactory.java
+++ src/main/CutGroupFactory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class CutGroupFactory {
 
-    static float[] pipeDiameters = {0.5f, 0.75f, 1.0f, 1.25f, 1.5f};
+    private static float[] pipeDiameters = {0.5f, 0.75f, 1.0f, 1.25f, 1.5f};
 
     public static ArrayList<CutGroup> getFilledCutGroups(ArrayList<PipeCut> pipeCuts) throws Exception {
         ArrayList<CutGroup> cutGroups = getEmptyCutGroups();
@@ -50,17 +50,6 @@ public class CutGroupFactory {
                 cutGroups.remove(cutGroup);
             }
         }
-    }
-
-    private static ArrayList<CutGroup> getNonEmptyCutGroups(ArrayList<CutGroup> inputGroups) {
-        ArrayList<CutGroup> nonEmptyGroups = new ArrayList<>();
-
-        for (CutGroup cutGroup : inputGroups) {
-            if (cutGroup.size() != 0)
-                nonEmptyGroups.add(cutGroup);
-        }
-
-        return nonEmptyGroups;
     }
 
 }
