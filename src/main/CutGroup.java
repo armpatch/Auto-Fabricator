fee90e1340700feb484ee788a033febbdfe27c43
Aaron <ampatch@gmail.com>
2019-08-30 07:47:49 -0400

    Batch: changed to Cutgroup and added more fields

--- src/main/CutGroup.java
+++ src/main/CutGroup.java
@@ -3,11 +3,15 @@ package main;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Batch {
+public class CutGroup {
 
     private ArrayList<PipeCut> pipeCuts;
 
     private UUID uuid;
 
-    private String service;
+    private String pipeService;
+    private String pipeDiameter;
+    private boolean isPulledTee;
+
+
 }
