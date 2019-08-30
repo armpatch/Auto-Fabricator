bfc011e248cc1a20ab62d62fec9826829c33a693
Aaron <ampatch@gmail.com>
2019-08-30 08:54:18 -0400

    CutGroupFactory: + getMatchingCutGroup(...) method

--- src/main/CutGroupFactory.java
+++ src/main/CutGroupFactory.java
@@ -6,8 +6,13 @@ public class CutGroupFactory {
 
     static float[] pipeDiameters = {0.5f, 0.75f, 1.0f, 1.25f, 1.5f};
 
-    public static ArrayList<CutGroup> getCutGroups(ArrayList<PipeCut> pipeCuts) {
-        ArrayList<CutGroup> cutGroups = new ArrayList<>();
+    public static ArrayList<CutGroup> getFilledCutGroups(ArrayList<PipeCut> pipeCuts) throws Exception {
+        ArrayList<CutGroup> cutGroups = getEmptyCutGroups();
+
+        for (PipeCut pipeCut: pipeCuts) {
+            CutGroup matchingCutGroup = getMatchingCutGroup(pipeCut, cutGroups);
+        }
+
 
 
     }
@@ -23,6 +28,16 @@ public class CutGroupFactory {
         }
 
         return emptyCutGroups;
+    }
+
+    private static CutGroup getMatchingCutGroup(PipeCut pipeCut, ArrayList<CutGroup> cutGroups) throws Exception {
+        for (CutGroup cutGroup: cutGroups) {
+            if (pipeCut.isPulledTee() == cutGroup.isPulledTee() &&
+            pipeCut.getDiameter() == cutGroup.getPipeDiameter()) {
+                return cutGroup;
+            }
+        }
+        throw new Exception("getMatchingCutGroup: Could not find a group that matches PipeCut");
     }
 
 
